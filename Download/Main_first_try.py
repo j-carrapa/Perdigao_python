1e0602d4f461980f06c5a2109de7b2c242113121
j-carrapa <104782915+j-carrapa@users.noreply.github.com>
2022-05-20 18:41:50 +0100

    Create Main_first_try.py

---
+++ Download/Main_first_try.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 20 16:15:06 2022
+
+@author: Joao
+"""
+
+# Main first try
+f_ans = input("Choose one option:\n[1] Manual sonic selection\n[2] All sonics in one mast\n[3]All sonics in choosen masts\n[4] All sonics for specified height\n[5] Sonics for specified height, in choosen masts\nPlease choose the number of the desired mode:")
+
+
+
+'''
+
+g1 = 0
+
+while g1 != 1:
+    
+    #all the code for 1 sonic
+    
+    g2 = 0
+    
+    while g2 != 1:
+        g_ans = input("End of data gathering?\n[yes/no]:")
+        
+        if g_ans == 'no':
+            g2 = 1
+            continue
+        if g_ans == 'yes':
+            g2 = 1
+            g1 = 1
+            continue
+        else:
+            print("Type correct answer.\n[yes/no]:")
+            continue
+    continue
+
+        
+'''
+    
+    
