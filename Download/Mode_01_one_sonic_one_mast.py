497d3879bbfc0ff4cc951b6c352d4afd69ac6584
j-carrapa <104782915+j-carrapa@users.noreply.github.com>
2022-06-07 10:49:30 +0100

    Create Mode_01_one_sonic_one_mast.py

---
+++ Download/Mode_01_one_sonic_one_mast.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  6 16:45:48 2022
+
+@author: João
+"""
+
+'''
+This module is used for describing how the app will work if mode 1 is selected
+If there is no way of running modules as scrips, this code will be copied to the main file
+'''
+
+# mode 1 one sonic height 1 mast
+
