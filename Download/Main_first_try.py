3b60a98b3b3048eebddcb09c974f406a07091757
j-carrapa <104782915+j-carrapa@users.noreply.github.com>
2022-06-09 22:53:41 +0100

    Update Main_first_try.py
    loop the input for multiple actions
    joining mode_2 module

--- Download/Main_first_try.py
+++ Download/Main_first_try.py
@@ -6,11 +6,41 @@ Created on Fri May 20 16:15:06 2022
 """
 
 # Main first try
-f_ans = input("Choose one option:\n[1] Manual sonic selection\n[2] All sonics in one mast\n[3]All sonics in choosen masts\n[4] All sonics for specified height\n[5] Sonics for specified height, in choosen masts\nPlease choose the number of the desired mode:")
-f_ans = int(f_ans)
 
-if f_ans == 1:
-    import Mode_01_one_sonic_one_mast
+g1 = 0
+
+while g1 != 1:
+    
+    g2 = 0
+    
+    f_ans = input("Choose one option:\n[1] Manual sonic selection\n[2] All sonics in one mast\n[3]All sonics in choosen masts\n[4] All sonics for specified height\n[5] Sonics for specified height, in choosen masts\nPlease choose the number of the desired mode:")
+    f_ans = int(f_ans)
+
+    if f_ans == 1:
+        import Mode_01_one_sonic_one_mast
+
+    if f_ans == 2:
+        import Mode_02_all_sonics_one_mast
+        
+    
+        
+    
+    while g2 != 1:
+        g_ans = input("End of data gathering?\n[yes/no]:")
+        
+        if g_ans == 'no':
+            g2 = 1
+            continue
+        if g_ans == 'yes':
+            g2 = 1
+            g1 = 1
+            continue
+        else:
+            print("Type correct answer.\n[yes/no]:")
+            continue
+    continue
+
+
 
 
 
