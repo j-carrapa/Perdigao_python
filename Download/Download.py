662ca93f5125cfa2f8a011679f050b2fb99825b3
j-carrapa <104782915+j-carrapa@users.noreply.github.com>
2022-06-09 22:50:44 +0100

    Update Download.py
    There was a problem with the for loop when dates_def seze =  1, now it is fixed

--- Download/Download.py
+++ Download/Download.py
@@ -17,12 +17,20 @@ def download (dates_def):
     print("Downloading...")
 
     # Just to explain user what is happening
-
-    for date in dates_def:
+    
+    if dates_def.size == 1:
+        date = int(dates_def)
         url = 'http://windsptds.fe.up.pt/thredds/fileServer/flux/NCAR-EOL%20Quality%20Controlled%205-minute%20ISFS%20surface%20flux%20data,%20geographic%20coordinate,%20tilt%20corrected/isfs_qc_tiltcor_'+ str(date) +'.nc'
         r = requests.get(url)
         open(str(date)+'.nc','wb').write(r.content)
+    else:
+        for date in dates_def:
+            url = 'http://windsptds.fe.up.pt/thredds/fileServer/flux/NCAR-EOL%20Quality%20Controlled%205-minute%20ISFS%20surface%20flux%20data,%20geographic%20coordinate,%20tilt%20corrected/isfs_qc_tiltcor_'+ str(date) +'.nc'
+            r = requests.get(url)
+            open(str(date)+'.nc','wb').write(r.content)
 
+
+    
 
 
 '''
