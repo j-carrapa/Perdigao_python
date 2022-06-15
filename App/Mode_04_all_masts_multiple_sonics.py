99534b42612a2c761597c23fe594992132d8df72
j-carrapa <104782915+j-carrapa@users.noreply.github.com>
2022-06-15 18:10:15 +0100

    Creation of App File
    Moving Perdigao App into a separate  folder

---
+++ App/Mode_04_all_masts_multiple_sonics.py
@@ -0,0 +1,261 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 10 11:17:11 2022
+
+@author: João
+"""
+
+# # Mode 4 - All masts in choosen heights
+
+from netCDF4 import Dataset
+import numpy as np
+import pandas as pd
+import Tower_location as tl
+import requests
+import Dates_array as da
+import Data_gathering as dg
+import Download as dl
+import save_export_function as se
+import Time_period_adjust as tp
+import Process_routines_compiled as pr
+
+'''----- PART 1 ----------'''
+
+# The array 'dates' will contain all the dates of the days with available data
+
+
+# Fetching the dates arrays from the Dates_array module
+
+dates_16 = da.d16
+dates_tot = da.d
+
+'''----- PART 2 ----------'''
+
+#'''
+# - Ask for the period time of the sample, multiples of 5 min: 5, 10, 15, 20, 30min or 1h, doesn´t accept other time periods
+p0 = 0
+
+while p0 != 1:
+    period_val = input("Type one of the options for time period in min:\n5, 10, 15, 20, 30 or 60:")
+    
+    p = int(period_val)
+    
+    if p == 5 or p == 10 or p == 15 or p == 20 or p == 30 or p == 60:
+        p0 = 1
+        continue
+    else:
+        print("Invalid time period")
+        continue
+  
+
+period_conv = int(period_val)/5
+
+k = period_conv
+
+
+#'''
+
+
+'''----- PART 3 ----------'''
+
+# Ask for input of which hours of the day are of interest
+# z3 = start hour
+# z3 = ending hour
+
+hour_arr = np.arange(0, 25)
+
+a7 = 0
+
+while a7 != 1:
+    z3 = input("Select start hour from 0-23:")
+    z3 = int(z3)
+    for c in hour_arr:
+        if z3 == c and z3 != 24:
+            a7 = 1
+            break
+    if a7 != 1:
+        print("Start hour incorrect")
+        print("Possible start hours:")
+        print(hour_arr[0:24])
+    continue
+
+a8 = 0
+
+while a8 != 1:
+    z4 = input("Select ending hour from {}-24:".format(z3+1))
+    z4 = int(z4)
+    for c in hour_arr:
+        if z4 == c and z4 > z3:
+            a8 = 1
+            break
+    if a8 != 1:
+        print("Ending hour incorrect")
+        print("Possible ending hours:")
+        print(hour_arr[z3+1:25])
+    continue
+
+
+'''----- PART 4 ----------'''
+
+# define an array that contains the dates defined by the user
+
+# Ask for input of time period
+
+a4 = 0
+
+while a4 != 1:
+    z1 = input("Select start date with the format, YYYYMMDD:")
+    z1 = int(z1)
+    for c in dates_tot:
+        if z1 == c:
+            a4 = 1
+            break
+    if a4 != 1:
+        print("Start date incorrect")
+        print("Possible start dates:")
+        print(dates_16)
+        print("And every date from 16 January 2017 to 1 July 2017")
+    continue
+
+a5 = 0
+
+while a5 != 1:
+    z2 = input("Select end date with the format, YYYYMMDD:")
+    z2 = int(z2)
+    for c in dates_tot:
+        if z2 == c:
+            if z2 >= z1:
+                a5 = 1
+                break
+            else:
+                print("End date can not be a previous date from the starting date")
+        
+            
+    if a5 != 1:
+        print("Start date incorrect")
+        print("Possible start dates:")
+    continue
+
+# using defined sart and end dates (z1, z2) to create an array with the defined dates
+# This for cycle is showing an error (dates_def appears first for append than for creation, the fact is that the if line for the append will not ever be executed before the if line for creation). 
+# Is it necessary to create an empty array first???
+#dates_def = np.array(0, dtype = 'i4')
+
+a6 = 0
+
+for q in dates_tot:
+    if a6 == 1:
+        temp = np.array(q, dtype='i4')
+        dates_def = np.append(dates_def, [temp])    
+    if z1 == q:
+        dates_def = np.array(z1, dtype = 'i4')
+        a6 = 1
+    if z2 == q:
+        a6 = 0
+
+# the array dates_def contains all the dates defined by the user for extracting data, every position of the array contains the date of 1 day with the format YYYYMMDD
+
+# Write an input extra function to add dates that are not sequential and to order them in dates_def array
+
+'''----- PART 5 ----------'''
+
+dl.download(dates_def)
+
+'''----- PART 6 ----------'''
+   
+# Create input section on wich sonics heights will be extracted
+    
+# Array with tower name code
+
+t_name = tl.t_n
+
+# Array with tower heights
+
+height = tl.h
+
+# Ask for input of sonic height, the code will not continue until a valid code name is given
+
+a2 = 0
+a3 = 0
+
+print("Type 1 sonic height and press enter.\nYou will be asked for a new sonic height until you type[end]")
+
+while a3 != 1:
+    i = 0
+    a1 = 0
+    y = input("Sonic height:")
+    for a in height:
+        
+        
+        if y == 'end':
+            a3 = 1
+            a1 = 1
+            break
+        
+        if y == a:
+            
+            if a2 == 1:
+                y_arr = np.append(y_arr, y)
+                i_arr = np.append(i_arr, i)
+                
+            if a2 == 0:
+                y_arr = np.array(y)
+                i_arr = np.array(i)
+                y0 = y
+                i0 = i
+                a2 = 1
+              
+            a1 = 1
+            break
+        i = i + 1
+    if a1 != 1:
+        print("Sonic height incorrect")
+        
+    continue
+
+
+# At this point we have the dates defined by the user and the sonics heights
+
+'''----- PARTS 7-11 ----------'''
+
+print("Gathering data and exporting")
+
+# Just to explain user what is happening
+
+
+if y_arr.size > 1:
+    
+    j = 0
+    for y in y_arr:
+        
+        i = i_arr.item(j)
+        
+        # Create an array with the towers available for that sonic height
+
+        tl.tower_avaliable_name(i)
+        tow = tl.tow.copy()
+
+        j += 1
+        
+        # Loop through all the available towers and call the process_routines function to process and export the data in each one of them, for the designated sonic height
+        for x in tow:
+            
+            # this function compiles every process of data gathering, calculate turbulence parameters, adjust the time of the samples, adjust the time period through the day and saving and exporting the data for 1 sonic in the designated time period.
+
+            pr.process_routines(dates_def, z1, z2, z3, z4, x, y, k)
+        
+else:
+    y = y0
+    i = i0
+    
+    # Create an array with the towers available for that sonic height
+
+    tl.tower_avaliable_name(i)
+    tow = tl.tow.copy()
+    
+    # Loop through all the available towers and call the process_routines function to process and export the data in each one of them, for the designated sonic height
+    for x in tow:
+        
+        # this function compiles every process of data gathering, calculate turbulence parameters, adjust the time of the samples, adjust the time period through the day and saving and exporting the data for 1 sonic in the designated time period.
+
+        pr.process_routines(dates_def, z1, z2, z3, z4, x, y, k)
---
+++ App/extract_function.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  6 15:08:07 2022
+
+@author: João
+"""
+
+from netCDF4 import Dataset
+import numpy as np
+import pandas as pd
+import time
+
+
+# Extracting from netcdf into a pandas data frame
+# Input args: fi - day code, x - tower code, y - sonic height, n - number of the day in the time series requested (if you want 2 consecutive days, in the first day n=0 and in the second day n=1)
+# Output: df - dataframe containing the variables of interest for that day, tower and sonic
+
+def extract (fi, x, y, n):
+    data = Dataset("{}.nc".format(fi) , 'r')
+    
+    # Storing the netCDF data into variables
+
+    u = data.variables['u_{}m_{}'.format(y, x)]
+    v = data.variables['v_{}m_{}'.format(y, x)]
+    w = data.variables['w_{}m_{}'.format(y, x)]
+
+    uu = data.variables['u_u__{}m_{}'.format(y, x)]
+    uv = data.variables['u_v__{}m_{}'.format(y, x)]
+    uw = data.variables['u_w__{}m_{}'.format(y, x)]
+    vv = data.variables['v_v__{}m_{}'.format(y, x)]
+    vw = data.variables['v_w__{}m_{}'.format(y, x)]
+    ww = data.variables['w_w__{}m_{}'.format(y, x)]
+
+    direc = data.variables['dir_{}m_{}'.format(y, x)]
+    spd = data.variables['spd_{}m_{}'.format(y, x)]
+ #   ldiag = data.variables['ldiag_{}m_{}'.format(y, x)]
+
+    basetime = data.variables['base_time']
+    reltime = data.variables['time']
+
+
+    # Creating an empty pandas dataframe
+    starting_time = data.variables['time'].units[14:29]+ '2:30'
+    ending_time = data.variables['time'].units[14:25]+ '23:57:30'
+    time_range = pd.date_range(start= starting_time, end= ending_time, periods= 288)
+
+    global df
+    
+    df = pd.DataFrame(0, columns= ['basetime', 'time', 'u','v', 'w', 'vh', 'dir','uu', 'vv', 'ww', 'uv', 'uw', 'vw'], index = time_range)
+
+    # Create a numpy array with the size of the time variable
+
+    dt = np.arange(0, data.variables['time'].size)
+
+    # Filling the empty pandas data frame with the values of the variables for each time value
+
+    for time_index in dt:
+        df.iloc[time_index] = basetime[time_index], reltime[time_index] + 86400*n, u[time_index], v[time_index], w[time_index], spd[time_index], direc[time_index], uu[time_index], vv[time_index], ww[time_index], uv[time_index], uw[time_index], vw[time_index]
+
+    return df
+    
+
+'''
+# Checking if the function is working - Apparently it is
+fi = '20170601'
+y = '10'
+x = 'tnw01'
+n = 1
+z1 = 20170601
+z2 = 20170603
+k= 2
+
+extract(fi, x, y, n)
+
+
+#'''
+
+def extract_2 (fi, x, y):
+    
+    data = Dataset("{}.nc".format(fi) , 'r')
+    global dt
+    dt = np.arange(0, data.variables['time'].size)
+    return dt
+    
+
+#extract_2(fi, x, y)
+
+def extract_start_time (z1, k, h):
+    
+    data = Dataset("{}.nc".format(z1) , 'r')
+    
+    st = 150*k + 3600*h
+    stf = time.strftime('%H:%M:%S', time.gmtime(st))
+    global start
+    start = data.variables['time'].units[14:25]+ stf
+    return start
+
+#extract_start_time(z1, k)
+
+def extract_end_time (z2, k, h):
+    
+    data = Dataset("{}.nc".format(z2) , 'r')
+    if h != 0:
+        en = 86400 - (150*k + (24-h)*3600)
+    else:
+        en = 86400 - (150*k)
+        
+    e = time.strftime('%H:%M:%S', time.gmtime(en))
+    global end
+    end = data.variables['time'].units[14:25]+ e
+    return end
+
+#extract_end_time(z2, k)
