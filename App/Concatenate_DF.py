fa2b631461316899aff1eafd5506346cf9fa098a
j-carrapa <104782915+j-carrapa@users.noreply.github.com>
2022-06-20 19:33:01 +0100

    Update Concatenate_DF.py
    Rewrite comments
    Clean unnecessary code

--- App/Concatenate_DF.py
+++ App/Concatenate_DF.py
@@ -5,7 +5,8 @@ Created on Tue Jun  7 10:35:30 2022
 @author: Joao
 """
 import pandas as pd
-# Concatenate 2 data frames of 1 sonic
+
+# Concatenate 2 data frames of 1 sonic into 1 data frame
 
 def concatenate (df1, df2):
     
@@ -16,25 +17,4 @@ def concatenate (df1, df2):
     result = pd.concat(frames)
     dfc1 = result.copy()
     
-            
     return dfc1
-
-
-'''
-def concatenate (df, dfc1, dates_def, m):
-    
-    global dfc2
-    for z in dates_def:
-        if m == 1:
-            frames.append(df)
-            result = pd.concat(frames)
-            dfc1 = result.copy()
-            frames = [dfc1]
-            
-        if m == 0:
-            dfc1 = df.copy()
-            frames = [dfc1]
-            m = 1
-    dfc2 = dfc1.copy()
-    return dfc2
-'''
