506a78601706fcc22c855ff99029281970c83911
j-carrapa <104782915+j-carrapa@users.noreply.github.com>
2022-06-20 19:34:22 +0100

    Update Download.py
    Rewrite comments
    Clean unnecessary code

--- App/Download.py
+++ App/Download.py
@@ -8,9 +8,6 @@ Created on Mon Jun  6 16:50:12 2022
 import requests
 import numpy as np
 
-
-#dates def as a function?
-
 # Download the netcd files defined by the user from perdigao site
 
 def download (dates_def):
@@ -28,14 +25,3 @@ def download (dates_def):
             url = 'http://windsptds.fe.up.pt/thredds/fileServer/flux/NCAR-EOL%20Quality%20Controlled%205-minute%20ISFS%20surface%20flux%20data,%20geographic%20coordinate,%20tilt%20corrected/isfs_qc_tiltcor_'+ str(date) +'.nc'
             r = requests.get(url)
             open(str(date)+'.nc','wb').write(r.content)
-
-
-    
-
-
-'''
-#Checking if the function is working - Apparently it is
-dates_def = np.array([20170601, 20170602, 20170603])
-
-download(dates_def)
-#'''
